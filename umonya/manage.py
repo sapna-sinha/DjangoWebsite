13b1c642643807e28a9843ead5ea2b3478b7297b
sapna-sinha <66251923+sapna-sinha@users.noreply.github.com>
2020-10-02 13:30:46 +0530

    Update manage.py

--- umonya/manage.py
+++ umonya/manage.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+#import os and sys
 import os
 import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "umonya.settings")
+    #import from django the execute_from_command_line
 
     from django.core.management import execute_from_command_line
 
